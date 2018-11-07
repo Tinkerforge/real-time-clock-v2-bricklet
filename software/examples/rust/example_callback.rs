95cfe9bdac2dacb5788fc3088b048d5a2cd8240a
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:26:24 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,38 +1,39 @@
 use std::{error::Error, io, thread};
-use tinkerforge::{ipconnection::IpConnection, real_time_clock_v2_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, real_time_clock_v2_bricklet::*};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Real-Time Clock Bricklet 2.0
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Real-Time Clock Bricklet 2.0.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let real_time_clock_v2_bricklet = RealTimeClockV2Bricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let rtc = RealTimeClockV2Bricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    //Create listener for date and time events.
-    let date_time_listener = real_time_clock_v2_bricklet.get_date_time_receiver();
-    // Spawn thread to handle received events. This thread ends when the real_time_clock_v2_bricklet
+    // Create receiver for date and time events.
+    let date_time_receiver = rtc.get_date_time_receiver();
+
+    // Spawn thread to handle received events. This thread ends when the `rtc` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
-        for event in date_time_listener {
-            println!("Year: {}", event.year);
-            println!("Month: {}", event.month);
-            println!("Day: {}", event.day);
-            println!("Hour: {}", event.hour);
-            println!("Minute: {}", event.minute);
-            println!("Second: {}", event.second);
-            println!("Centisecond: {}", event.centisecond);
-            println!("Weekday: {}", event.weekday);
-            println!("Timestamp: {}", event.timestamp);
+        for date_time in date_time_receiver {
+            println!("Year: {}", date_time.year);
+            println!("Month: {}", date_time.month);
+            println!("Day: {}", date_time.day);
+            println!("Hour: {}", date_time.hour);
+            println!("Minute: {}", date_time.minute);
+            println!("Second: {}", date_time.second);
+            println!("Centisecond: {}", date_time.centisecond);
+            println!("Weekday: {}", date_time.weekday);
+            println!("Timestamp: {}", date_time.timestamp);
             println!();
         }
     });
 
-    // Set period for date and time callback to 5s (5000ms)
-    real_time_clock_v2_bricklet.set_date_time_callback_configuration(5000);
+    // Set period for date and time callback to 5s (5000ms).
+    rtc.set_date_time_callback_configuration(5000);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,20 +1,20 @@
 use std::{error::Error, io};
 
-use tinkerforge::{ipconnection::IpConnection, real_time_clock_v2_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, real_time_clock_v2_bricklet::*};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Real-Time Clock Bricklet 2.0
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Real-Time Clock Bricklet 2.0.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let real_time_clock_v2_bricklet = RealTimeClockV2Bricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let rtc = RealTimeClockV2Bricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    // Get current date and time
-    let get_date_time_result = real_time_clock_v2_bricklet.get_date_time().recv()?;
+    // Get current date and time.
+    let get_date_time_result = rtc.get_date_time().recv()?;
 
     println!("Year: {}", get_date_time_result.year);
     println!("Month: {}", get_date_time_result.month);
@@ -24,7 +24,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("Second: {}", get_date_time_result.second);
     println!("Centisecond: {}", get_date_time_result.centisecond);
     println!("Weekday: {}", get_date_time_result.weekday);
-    println!("Timestamp: {}{}", get_date_time_result.timestamp, " ms");
+    println!("Timestamp: {} ms", get_date_time_result.timestamp);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
