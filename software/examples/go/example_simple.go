d122dc3c068b5489346a878bf4b4135d0eb7d78a
Erik Fleckstein <erik@tinkerforge.com>
2018-12-18 14:59:47 +0100

    go: Change import path.

--- software/examples/go/example_simple.go
+++ software/examples/go/example_simple.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"tinkerforge/ipconnection"
-	"tinkerforge/real_time_clock_v2_bricklet"
+	"github.com/tinkerforge/go-api-bindings/ipconnection"
+	"github.com/tinkerforge/go-api-bindings/real_time_clock_v2_bricklet"
 )
 
 const ADDR string = "localhost:4223"
