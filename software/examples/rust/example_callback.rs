95990a246b00ee188a21ef4477a4a050f98fc79d
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:39:30 +0100

    rust: Reformat examples.

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,5 +1,8 @@
-use std::{error::Error, io, thread};
-use tinkerforge::{ip_connection::IpConnection, real_time_clock_v2_bricklet::*};
+use std::{io, error::Error};
+use std::thread;
+use tinkerforge::{ip_connection::IpConnection, 
+                  real_time_clock_v2_bricklet::*};
+
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -10,6 +13,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let rtc = RealTimeClockV2Bricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+<<<<<<< Updated upstream
                                           // Don't use device before ipcon is connected.
 
     let date_time_receiver = rtc.get_date_time_callback_receiver();
@@ -50,6 +54,54 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // Set period for date and time callback to 5s (5000ms).
     rtc.set_date_time_callback_configuration(5000);
+=======
+    // Don't use device before ipcon is connected.
+
+     let date_time_receiver = rtc.get_date_time_callback_receiver();
+
+        // Spawn thread to handle received callback messages. 
+        // This thread ends when the `rtc` object
+        // is dropped, so there is no need for manual cleanup.
+        thread::spawn(move || {
+            for date_time in date_time_receiver {           
+                		println!("Year: {}", date_time.year);
+		println!("Month: {}", date_time.month);
+		println!("Day: {}", date_time.day);
+		println!("Hour: {}", date_time.hour);
+		println!("Minute: {}", date_time.minute);
+		println!("Second: {}", date_time.second);
+		println!("Centisecond: {}", date_time.centisecond);
+
+		if date_time.weekday == REAL_TIME_CLOCK_V2_BRICKLET_WEEKDAY_MONDAY { 
+			println!("Weekday: Monday");
+		}
+		else if date_time.weekday == REAL_TIME_CLOCK_V2_BRICKLET_WEEKDAY_TUESDAY { 
+			println!("Weekday: Tuesday");
+		}
+		else if date_time.weekday == REAL_TIME_CLOCK_V2_BRICKLET_WEEKDAY_WEDNESDAY { 
+			println!("Weekday: Wednesday");
+		}
+		else if date_time.weekday == REAL_TIME_CLOCK_V2_BRICKLET_WEEKDAY_THURSDAY { 
+			println!("Weekday: Thursday");
+		}
+		else if date_time.weekday == REAL_TIME_CLOCK_V2_BRICKLET_WEEKDAY_FRIDAY { 
+			println!("Weekday: Friday");
+		}
+		else if date_time.weekday == REAL_TIME_CLOCK_V2_BRICKLET_WEEKDAY_SATURDAY { 
+			println!("Weekday: Saturday");
+		}
+		else if date_time.weekday == REAL_TIME_CLOCK_V2_BRICKLET_WEEKDAY_SUNDAY { 
+			println!("Weekday: Sunday");
+		}
+
+		println!("Timestamp: {}", date_time.timestamp);
+		println!();
+            }
+        });
+
+		// Set period for date and time callback to 5s (5000ms).
+		rtc.set_date_time_callback_configuration(5000);
+>>>>>>> Stashed changes
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,6 +1,8 @@
-use std::{error::Error, io};
+use std::{io, error::Error};
 
-use tinkerforge::{ip_connection::IpConnection, real_time_clock_v2_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, 
+                  real_time_clock_v2_bricklet::*};
+
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -11,6 +13,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let rtc = RealTimeClockV2Bricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+<<<<<<< Updated upstream
                                           // Don't use device before ipcon is connected.
 
     // Get current date and time.
@@ -41,6 +44,44 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     println!("Timestamp: {} ms", date_time.timestamp);
+=======
+    // Don't use device before ipcon is connected.
+
+		// Get current date and time.
+let date_time = rtc.get_date_time().recv()?;
+
+		println!("Year: {}", date_time.year);
+		println!("Month: {}", date_time.month);
+		println!("Day: {}", date_time.day);
+		println!("Hour: {}", date_time.hour);
+		println!("Minute: {}", date_time.minute);
+		println!("Second: {}", date_time.second);
+		println!("Centisecond: {}", date_time.centisecond);
+
+		if date_time.weekday == REAL_TIME_CLOCK_V2_BRICKLET_WEEKDAY_MONDAY {
+			println!("Weekday: Monday");
+		}
+		else if date_time.weekday == REAL_TIME_CLOCK_V2_BRICKLET_WEEKDAY_TUESDAY {
+			println!("Weekday: Tuesday");
+		}
+		else if date_time.weekday == REAL_TIME_CLOCK_V2_BRICKLET_WEEKDAY_WEDNESDAY {
+			println!("Weekday: Wednesday");
+		}
+		else if date_time.weekday == REAL_TIME_CLOCK_V2_BRICKLET_WEEKDAY_THURSDAY {
+			println!("Weekday: Thursday");
+		}
+		else if date_time.weekday == REAL_TIME_CLOCK_V2_BRICKLET_WEEKDAY_FRIDAY {
+			println!("Weekday: Friday");
+		}
+		else if date_time.weekday == REAL_TIME_CLOCK_V2_BRICKLET_WEEKDAY_SATURDAY {
+			println!("Weekday: Saturday");
+		}
+		else if date_time.weekday == REAL_TIME_CLOCK_V2_BRICKLET_WEEKDAY_SUNDAY {
+			println!("Weekday: Sunday");
+		}
+
+		println!("Timestamp: {} ms", date_time.timestamp);
+>>>>>>> Stashed changes
 
     println!("Press enter to exit.");
     let mut _input = String::new();
