6b71776c674f3f469441f7301038b3f704844228
Matthias Bolte <matthias@tinkerforge.com>
2022-04-27 15:37:42 +0200

    uc: Adapt to bindings changes

--- software/examples/uc/example_callback.c
+++ software/examples/uc/example_callback.c
@@ -1,17 +1,13 @@
 // This example is not self-contained.
-// It requres usage of the example driver specific to your platform.
+// It requires usage of the example driver specific to your platform.
 // See the HAL documentation.
 
-#include "bindings/hal_common.h"
-#include "bindings/bricklet_real_time_clock_v2.h"
+#include "src/bindings/hal_common.h"
+#include "src/bindings/bricklet_real_time_clock_v2.h"
 
-#define UID "XYZ" // Change XYZ to the UID of your Real-Time Clock Bricklet 2.0
-
-void check(int rc, const char* msg);
-
+void check(int rc, const char *msg);
 void example_setup(TF_HAL *hal);
 void example_loop(TF_HAL *hal);
-
 
 // Callback function for date and time callback
 static void date_time_handler(TF_RealTimeClockV2 *device, uint16_t year, uint8_t month,
@@ -52,7 +48,7 @@ static TF_RealTimeClockV2 rtc;
 
 void example_setup(TF_HAL *hal) {
 	// Create device object
-	check(tf_real_time_clock_v2_create(&rtc, UID, hal), "create device object");
+	check(tf_real_time_clock_v2_create(&rtc, NULL, hal), "create device object");
 
 	// Register date and time callback to function date_time_handler
 	tf_real_time_clock_v2_register_date_time_callback(&rtc,
